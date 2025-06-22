4b0e92f4ec91f05e6ac9dd9d5c169bf6fa081b67
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-06-22 08:57:37 -0400

    mainly change link to the "pay" page rather than home

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -5,15 +5,12 @@ import type { ReactElement } from "react";
 
 export default function HistoryPage(): ReactElement {
     return (
-        <main className="max-w-2xl mx-auto px-4 py-10 space-y-8 text-[var(--foreground)] min-h-screen">
+        <main className="min-h-screen max-w-2xl mx-auto px-4 py-10 space-y-8 text-[var(--foreground)]">
             <div className="flex justify-between items-center">
-                <h1 className="text-3xl sm:text-4xl font-extrabold text-center font-poppins">
+                <h1 className="text-3xl sm:text-4xl font-extrabold tracking-tight text-center font-poppins">
                     History
                 </h1>
-                <Link
-                    href="/"
-                    className="btn btn-sm btn-outline"
-                >
+                <Link href="/pay" className="btn btn-sm btn-outline">
                     New Calculation
                 </Link>
             </div>
